f90dfdf6b97e56fa923c1e5f77832b3247fdc425
Brendan Thompson <brendan.thompson@ezidebit.com.au>
2014-03-21 14:23:55 +1000

    Created POST portion for the nodejs rest-app

--- node/rest-app.js
+++ node/rest-app.js
@@ -1,11 +1,50 @@
 var Client = require('node-rest-client').Client;
+var baseURL = "https://www.skyparse.com/api/hrxml?";
+var filePath = "C:\\tmp\\edms.docx";
 
-client = new Client(); // New instance of the rest client
+function encodeFile(filePath){
+	var fs = require('fs');
+	var encodedFile;
 
-args = {
-	path: { "accountID": '13213', "secretKey": 'test', "parseID": '1008'} // Setup the path variables
-};
+	console.log(filePath);
 
-client.get("https://www.skyparse.com/api/hrxml?accountid=${accountID}&secretkey=${secretKey}&parseid=${parseID}", args, function(data, response) { // Make rest call
-	console.log(data); // Return results to console
-});
+	fs.readFile(filePath, function(err, data) {
+		encodedFile = new Buffer(data).toString('base64');
+	})
+
+	return encodedFile;
+}
+
+function getCV() {
+	var client = new Client(); // New instance of the rest client
+	
+	args = {
+		path: { "accountID": '13213', "secretKey": 'test', "parseID": '1008'} // Setup the path variables
+	};
+	
+	client.get(baseURL + "accountid=${accountID}&secretkey=${secretKey}&parseid=${parseID}", args, function(data, response) { // Make rest call
+		console.log(data); // Return results to console
+	});
+}
+
+function postCV() {
+	var client = new Client();
+
+	args = {
+		path: {
+			"accountID": "13213",
+			"secretKey": "test",
+			"fileName": "edms.docx"
+		},
+		data: encodeFile(filePath)
+	};
+
+	client.post(baseURL + "accountid=${accountID}&secretkey=${secretKey}&filename=${fileName}", args, function(data, res) {
+		/*optional stuff to do after success */
+		console.log(data);
+	});
+}
+
+postCV();
+
+// encodeFile(filePath);
